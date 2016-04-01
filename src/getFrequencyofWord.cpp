f83ef143fa72d1cddc32b7a316e497a6f88e31b8
kavitha565 <kavitha565@missionrnd.com>
2016-04-01 05:25:39 -0700

    Submit Commit

--- src/getFrequencyofWord.cpp
+++ src/getFrequencyofWord.cpp
@@ -15,26 +15,45 @@ Note: Dont modify original str or word,Just return count ,Spaces can also be par
 int count_word_in_str_way_1(char *str, char *word){
 	if (str == NULL || word == NULL)
 		return 0;
-	int i, j, count = 0, flag = 0;
-	int strln = strlen(str);
-	int wordln = strlen(word);
-	for (i = 0; i < strln;)
+	int strln, wordln, i, j, k, flag = 0, count = 0;
+	strln = strlen(str);
+	wordln = strlen(word);
+
+	for (i = 0; i<strln; i++)
 	{
-		j = 0;
-		count = 0;
-		while ((str[i] == word[j]))
+		if (str[i] == word[0] && ((str[i - 1] == ' ' || i == 0) && (str[i + wordln] == ' ' || str[i + wordln] == '\0')))
 		{
-			flag++;
-			i++;
-			j++;
-		}
-		if (count == wordln)
-		{
-			count++;
-			flag = 0;
+			k = i + 1;
+			for (k = i + 1, j = 1; j<wordln;)
+			{
+				if (str[k++] == word[j++])
+				{
+					flag++;
+				}
+			}
+			if (flag == wordln - 1)
+			{
+				count++;
+				flag == 0;
+			}
 		}
 		else
-			i++;
+		{
+			flag = 0;
+			k = i + 1;
+			for (j = 1; j<wordln;)
+			{
+				if (str[k++] == word[j++])
+				{
+					flag++;
+				}
+			}
+			if (flag == wordln - 1)
+			{
+				count++;
+				flag == 0;
+			}
+		}
 	}
 	if (count != 0)
 		return count;
